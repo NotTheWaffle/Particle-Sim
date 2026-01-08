3d41033c5e1504d2987ccb512ea239ba1eedc7a8
NotTheWaffle <andrew.robson09@gmail.com>
2026-01-08 08:07:41 -0600

    psuh

--- Main.java
+++ Main.java
@@ -3,7 +3,7 @@ import java.util.concurrent.locks.LockSupport;
 
 public class Main {
 	public static void main(String[] args) {
-		new Thread(() -> runGame(new ParticleGame(256, 256), 60, 600)).start();
+		new Thread(() -> runGame(new ParticleGame(256, 256), 60, 599)).start();
 	}
 	public static void runGame(final Game game, final double fps, final double tps){
 		final Window window = new Window(game);
